485bf0f46419088ef4c28b081d7d874f2b9f9140
ComfortCodes <comfortcodes@gmail.com>
2020-07-09 10:47:24 +0100

    Rendering multiple elements using React.Fragment

---
+++ src/class.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class ReactState extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {outputText :'Placeholder'};
+    }
+    UpdateText = () =>{
+        this.setState({
+            outputText : document.getElementById('inputText').value
+        });
+    }
+    render(){
+        return(
+         <React.Fragment>
+             <input id='inputText' type='text'></input>
+             <button type='button' 
+             onClick ={this.UpdateText}>
+                 Update
+             </button>
+             <br/>
+             <label id='output'>
+                 {this.state.outputText}
+             </label>
+         </React.Fragment>   
+        );
+    }
+}
+export default ReactState;
